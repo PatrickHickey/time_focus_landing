5c25bc2139339c7f0496cce35fe6d89b24dde88f
PatrickHickey <codewithpat@gmail.com>
2019-08-13 20:27:01 -0700

    Added backround and greeting functionality

--- js/main.js
+++ js/main.js
@@ -30,5 +30,27 @@ function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+// Set Background and Greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage = "url('../img/morning.jpg')";
+    greeting.textContent = 'Good Morning';
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+    greeting.textContent = 'Good Afternoon';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = "url('../img/night.jpg')";
+    greeting.textContent = 'Good Evening';
+    document.body.style.color = 'white';
+  }
+}
+
 // Run
 showTime();
+setBgGreet();
